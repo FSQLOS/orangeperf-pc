2582dfd0f17173d32a2a89d25d9a09d44671954f
FSQLOS <fsqlnico@gmail.com>
2026-03-20 17:17:08 +0100

    Delete App.jsx

--- App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.css';
-import MobileDashboard from './MobileDashboardDesktop';
-import { config } from './config';
-
-export default function App() {
-  return <MobileDashboard config={config} />;
-}
